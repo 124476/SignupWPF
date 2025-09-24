e20d9c0c65b8e2e9edb45b0b85b1cdc46a93c7e3
124476 <12447698@bk.ru>
2025-09-24 12:49:23 +0300

    Uplode: form signup/login

---
+++ Desktop/Desktop/Pages/PageLogin.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Desktop.Pages
+{
+    public partial class PageLogin : Page
+    {
+        private bool _isAnimating = false;
+
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnSignup_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isAnimating) return;
+            _isAnimating = true;
+            
+            var fadeOutLoginRight = (Storyboard)FindResource("FadeOut");
+            fadeOutLoginRight.Begin(StackLoginRigth);
+
+            var fadeOutLoginLeft = (Storyboard)FindResource("FadeOut");
+            fadeOutLoginLeft.Begin(StackLoginLeft);
+
+            await Task.Delay(200);
+
+            StackLoginLeft.Visibility = Visibility.Collapsed;
+            StackLoginRigth.Visibility = Visibility.Collapsed;
+
+            StackSignupLeft.Visibility = Visibility.Visible;
+            StackSignupRigth.Visibility = Visibility.Visible;
+
+            var slideLeft = (Storyboard)FindResource("SlideLeftToRight");
+            slideLeft.Begin(StackSignupRigth);
+
+            var slideRight = (Storyboard)FindResource("SlideRightToLeft");
+            slideRight.Begin(StackSignupLeft);
+
+            await Task.Delay(300);
+            _isAnimating = false;
+        }
+
+        private async void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isAnimating) return;
+            _isAnimating = true;
+
+            var fadeOutSignupRight = (Storyboard)FindResource("FadeOut");
+            fadeOutSignupRight.Begin(StackSignupRigth);
+
+            var fadeOutSignupLeft = (Storyboard)FindResource("FadeOut");
+            fadeOutSignupLeft.Begin(StackSignupLeft);
+
+            await Task.Delay(200);
+
+            StackSignupLeft.Visibility = Visibility.Collapsed;
+            StackSignupRigth.Visibility = Visibility.Collapsed;
+
+            StackLoginLeft.Visibility = Visibility.Visible;
+            StackLoginRigth.Visibility = Visibility.Visible;
+
+            var slideLeft = (Storyboard)FindResource("SlideLeftToRight");
+            slideLeft.Begin(StackLoginLeft);
+
+            var slideRight = (Storyboard)FindResource("SlideRightToLeft");
+            slideRight.Begin(StackLoginRigth);
+
+            await Task.Delay(300);
+            _isAnimating = false;
+        }
+    }
+}
---
+++ Desktop/Desktop/PasswordBoxHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Desktop
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.RegisterAttached("PlaceholderText", typeof(string),
+                typeof(PasswordBoxHelper), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty IsPasswordEmptyProperty =
+            DependencyProperty.RegisterAttached("IsPasswordEmpty", typeof(bool),
+                typeof(PasswordBoxHelper), new PropertyMetadata(true));
+
+        public static string GetPlaceholderText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PlaceholderTextProperty);
+        }
+
+        public static void SetPlaceholderText(DependencyObject obj, string value)
+        {
+            obj.SetValue(PlaceholderTextProperty, value);
+        }
+
+        public static bool GetIsPasswordEmpty(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsPasswordEmptyProperty);
+        }
+
+        public static void SetIsPasswordEmpty(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsPasswordEmptyProperty, value);
+        }
+
+        static PasswordBoxHelper()
+        {
+            EventManager.RegisterClassHandler(typeof(PasswordBox),
+                PasswordBox.PasswordChangedEvent,
+                new RoutedEventHandler(OnPasswordChanged));
+        }
+
+        private static void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                SetIsPasswordEmpty(passwordBox, string.IsNullOrEmpty(passwordBox.Password));
+            }
+        }
+    }
+}
